547ae32b4587dbfa7aa32db2c5406ef941d68d4f
Deveshi Dwivedi <deveshigurgaon@gmail.com>
2024-12-04 19:27:56 +0530

    add project

---
+++ src/components/watch/VideoJSPlayer.tsx
@@ -0,0 +1,56 @@
+import { useEffect, useRef } from "react";
+import Player from "video.js/dist/types/player";
+import videojs from "video.js";
+import "videojs-youtube";
+import "video.js/dist/video-js.css";
+
+export default function VideoJSPlayer({
+  options,
+  onReady,
+}: {
+  options: any;
+  onReady: (player: Player) => void;
+}) {
+  const videoRef = useRef<HTMLDivElement | null>(null);
+  const playerRef = useRef<Player | null>(null);
+
+  useEffect(() => {
+    (async function handleVideojs() { 
+      if (!playerRef.current) { 
+        const videoElement = document.createElement("video-js"); 
+
+        videoRef.current?.appendChild(videoElement);
+        const player = (playerRef.current = videojs(
+          videoElement,
+          options,
+          () => {
+            onReady && onReady(player);
+          }
+        ));
+
+       
+      } else {
+        const player = playerRef.current;
+        player.width(options.width);
+        player.height(options.height);
+      }
+    })();
+  }, [options, videoRef]);
+
+  useEffect(() => {
+    const player = playerRef.current;
+
+    return () => {
+      if (player && !player.isDisposed()) {
+        player.dispose();
+        playerRef.current = null;
+      }
+    };
+  }, [playerRef]);
+
+  return (
+    <div data-vjs-player>
+      <div ref={videoRef} />
+    </div>
+  );
+}
